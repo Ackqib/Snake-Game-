2a079039cf5c6dde1eed382ac6a3edad7ab28194
Ahmed Aaqib <ahmedvecc@gmail.com>
2023-12-28 21:16:23 +0530

    Initial Commit

---
+++ snek.py
@@ -0,0 +1,10 @@
+# Generate a board which could be 8x8
+# Generate a snake that initially fits 1 tile
+# Initialize a tick cycle in which the board refreshes every 0.5 seconds 
+# Get user input to move snake left , right , center
+# Replace snake position to 1 tile in chosen direction 
+# If no input, continue moving in that direction or initial position
+# If APPLE + Snake is present on the same tile -> Increase snake length by 1 tile parrelel to last tile of snake 
+# If 2 snake tiles are present on 1 tile ; end game
+# If snake tile hit 
+
